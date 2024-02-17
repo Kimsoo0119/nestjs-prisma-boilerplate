4f623dd8fbd48b0fb92c903fb0991984adedfa00
Kimsoo0119 <96464209+Kimsoo0119@users.noreply.github.com>
2024-02-17 21:28:10 +0900

    Update excute.sh

--- scripts/excute.sh
+++ scripts/excute.sh
@@ -2,9 +2,9 @@
 
 cd /home/ubuntu/backend
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" 
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  
+npm -v
+
+pwd
+
+nvm -v
       
-npm ci
-sh scripts/docker-script.sh
